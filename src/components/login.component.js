7c4064ae9d3827de7e72803b10e848dbac1c9e51
Rami Del Toro <rami-deltoro@users.noreply.github.com>
2021-08-21 17:56:53 -0400

    ReactJs - Loging Page

--- src/components/login.component.js
+++ src/components/login.component.js
@@ -1,19 +1,76 @@
 import React, { Component } from "react";
+import jsSha1 from "js-sha1";
 
 export default class Login extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            userCredentials: {
+                emailAddress: props.emailAddress,
+                passwordHash: props.password
+            },
+
+            signInStatus : ''
+
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleEmailAddressUpdate(event) {
+        const userCredentials = this.state.userCredentials;
+        userCredentials.emailAddress = event.target.value;
+
+        this.setState({
+            userCredentials: userCredentials
+        });
+    }
+
+    handlePasswordUpdate(event) {
+        const userCredentials = this.state.userCredentials;
+        var password = event.target.value
+        userCredentials.passwordHash = jsSha1(password);
+
+        this.setState({
+            userCredentials: userCredentials
+        });
+    }
+
+    handleSubmit(event) {
+        console.log('A user attempted to log In ' + JSON.stringify(this.state.userCredentials));
+        this.checkUserCredentials(this.state.userCredentials);
+        event.preventDefault();
+    }
+
+    checkUserCredentials(body) {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(body)
+        };
+
+        return fetch('http://127.0.0.1:8080/signin', requestOptions)
+            .then(response => response.json())
+            .then(data => this.setState({ signInStatus: data['isValidUser'] }))
+            .then(data =>  console.log('Is valid user? ' + this.state['signInStatus'])); //async
+    }
+
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <h3>Sign In</h3>
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="email" className="form-control" placeholder="Enter email" onChange={this.handleEmailAddressUpdate.bind(this)}/>
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password" className="form-control" placeholder="Enter password" onChange={this.handlePasswordUpdate.bind(this)}/>
                 </div>
 
                 <div className="form-group">
--- src/components/signup.component.js
+++ src/components/signup.component.js
@@ -1,29 +1,101 @@
 import React, { Component } from "react";
+import jsSha1, { } from "js-sha1";
 
 export default class SignUp extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            signupForm: {
+                firstName: props.firstName,
+                lastName: props.lastName,
+                emailAddress: props.emailAddress,
+                password: props.password
+            },
+            signupStatus : ''
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleFirstNameUpdate(event) {
+        const signupForm = this.state.signupForm;
+        signupForm.firstName = event.target.value;
+
+        this.setState({
+           signupForm: signupForm
+        });
+    }
+
+    handleLastNameUpdate(event) {
+        const signupForm = this.state.signupForm;
+        signupForm.lastName = event.target.value;
+
+        this.setState({
+            signupForm: signupForm
+        });
+    }
+
+    handleEmailAddressUpdate(event) {
+        const signupForm = this.state.signupForm;
+        signupForm.emailAddress = event.target.value;
+
+        this.setState({
+            signupForm: signupForm
+        });
+    }
+
+    handlePasswordUpdate(event) {
+        const signupForm = this.state.signupForm;
+        signupForm.password = jsSha1(event.target.value);
+
+        this.setState({
+            signupForm: signupForm
+        });
+    }
+
+    handleSubmit(event) {
+        console.log('A user signed up ' + JSON.stringify(this.state.signupForm));
+        this.sendSignupFormToBackend(this.state.signupForm);
+        event.preventDefault();
+    }
+
+    sendSignupFormToBackend(body) {
+        const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(body)
+        };
+
+        fetch('http://127.0.0.1:8080/signup', requestOptions)
+            .then(response => response.json())
+            .then(data => this.setState({ signupStatus: data['signupStatus'] })); //async
+    }
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <h3>Sign Up</h3>
 
                 <div className="form-group">
                     <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
+                    <input type="text" className="form-control" placeholder="First name"  onChange={this.handleFirstNameUpdate.bind(this)} />
                 </div>
 
                 <div className="form-group">
                     <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
+                    <input type="text" className="form-control" placeholder="Last name"  onChange={this.handleLastNameUpdate.bind(this)}/>
                 </div>
 
                 <div className="form-group">
                     <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
+                    <input type="email" className="form-control" placeholder="Enter email"  onChange={this.handleEmailAddressUpdate.bind(this)}/>
                 </div>
 
                 <div className="form-group">
                     <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
+                    <input type="password" className="form-control" placeholder="Enter password" onChange={this.handlePasswordUpdate.bind(this)}/>
                 </div>
 
                 <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
